dbb9940fc8283d75ccc36bb3d2433a66b35beab4
ANA <127270321+anabeatrizlds@users.noreply.github.com>
2023-06-30 11:04:31 -0300

    Add files via upload

---
+++ aula.js
@@ -0,0 +1,10 @@
+console.log("Primeiro programa em java");
+const nome= "Maria";
+const sobrenome="Freitas"
+const idade= 25;
+
+console.log(nome, sobrenome);
+console.log(idade+2);
+const n1 = 10;
+const n2 = 20;
+const media = ( (n1+n2)/2);
